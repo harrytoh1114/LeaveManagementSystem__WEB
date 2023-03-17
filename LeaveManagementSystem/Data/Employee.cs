ae3734c0bbecda79cb0f7390d982b1e55e06c5d7
harrytoh1114 <laughing1114@gmail.com>
2023-03-17 16:22:45 +0800

    Added Database Tables and Migration. End Implement Database Design

---
+++ LeaveManagementSystem/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagementSystem.Data
+{
+    public class Employee : IdentityUser
+    {
+       public string? FirstName { get; set; }
+       public string? LastName { get; set; }
+       public string? TaxId { get; set; }
+       public DateTime DOB { get; set; }
+       public DateTime DateJoined { get; set; }
+
+    }
+}
